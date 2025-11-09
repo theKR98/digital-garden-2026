47b2f4746d6d36439e5c61e0b8d11f0621814c4a
Kxran <kirankrr@hotmail.com>
2025-11-09 12:16:06 +0800

    Quartz sync: Nov 9, 2025, 12:16 PM

---
+++ content/index.md
@@ -0,0 +1,9 @@
+---
+title: Welcome to Quartz
+---
+
+This is a blank Quartz installation.dddfuck
+See the [documentation](https://quartz.jzhao.xyz) for how to get started.
+
+
+dadad
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,8 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Kiran",
+    displayPicture: "",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
